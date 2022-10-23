3110fb317a75ca9499a819e38047f8999feacbb7
myaak <myakushek@outlook.com>
2022-10-23 17:28:34 +0300

    added scss

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,17 @@
 import {useState} from 'react'
+import './styles.scss'
 
 const Navbar = () => {
   const [search, setSearch] = useState<string>()
 
   return (
-    <div style={styles.wrapper} className="navbar__wrapper">
-      <div style={styles.navbar} className="navbar">
-        <div style={styles.logo} className="navbar__logo">
-          <img style={{
-            width:'200px',
-            height:'72px'
-          }} src="logo.jpg"/>
+    <div className="navbar__wrapper">
+      <div className="navbar">
+        <div className="navbar__logo">
+          <img src="logo.jpg"/>
         </div>
-          <div style={styles.search} className="navbar__searchbar">
-          <input type="text" value={search} style={styles.searchBar}
+          <div className="navbar__searchbar">
+          <input type="text" value={search}
             placeholder = "Search by pokemon name"
             onChange={(e) => setSearch(e.target.value)}
             />
@@ -24,34 +22,6 @@ const Navbar = () => {
 }
 
 
-const styles = {
-  wrapper :{
-    width:'100vw',
-    height:'72px',
-    backgroundColor: 'black'
-  },
-  navbar: {
-    display: 'flex',
-    width:'1200px',
-    height:'100%',
-    margin:'0 auto'
-  },
-  logo: {
-
-  },
-  search: {
-
-  },
-  searchBar: {
-    backgroundColor: 'transparent',
-    border: 'none',
-    borderBottom: '2px solid white',
-    color: 'white',
-    width: '300px',
-    height: '30px'
-  }
-
-}
 
 
 
