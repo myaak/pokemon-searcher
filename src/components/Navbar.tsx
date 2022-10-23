517bed9714f662e5c98b3c16178088639975ca75
myaak <myakushek@outlook.com>
2022-10-23 18:03:06 +0300

    completed nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,11 +10,24 @@ const Navbar = () => {
         <div className="navbar__logo">
           <img src="logo.jpg"/>
         </div>
-          <div className="navbar__searchbar">
+        <div className="navbar__searchbar">
           <input type="text" value={search}
             placeholder = "Search by pokemon name"
-            onChange={(e) => setSearch(e.target.value)}
-            />
+            onChange={(e) => setSearch(e.target.value)}/>
+            <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="" stroke-linecap="round" stroke-linejoin="round" className="css-i6dzq1">
+              <circle cx="11" cy="11" r="8"></circle>
+              <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+            </svg>
+        </div>
+         <div className="navbar__menu">
+          <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" className="css-i6dzq1">
+            <line x1="3" y1="12" x2="21" y2="12">
+            </line>
+            <line x1="3" y1="6" x2="21" y2="6">
+            </line>
+            <line x1="3" y1="18" x2="21" y2="18">
+            </line>
+          </svg>
         </div>
       </div>
     </div>
